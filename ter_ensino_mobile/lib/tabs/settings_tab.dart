72dbfa81f33deb9526edf9c5185288b774d9fdd3
lucsscarvalho <lucas.carvalhocco@gmail.com>
2020-07-03 08:42:26 -0300

    update settings

--- ter_ensino_mobile/lib/tabs/settings_tab.dart
+++ ter_ensino_mobile/lib/tabs/settings_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ter_ensino_mobile/screens/login_screen.dart';
+import 'package:ter_ensino_mobile/tiles/settings_tile.dart';
 
 class SettingsTab extends StatefulWidget {
   @override
@@ -14,40 +15,50 @@ class _SettingsTabState extends State<SettingsTab> {
       body: Container(
         alignment: Alignment.center,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Desenvolvido com 💚 por terEnsino © 2020.',
-                style: TextStyle(color: Colors.white),
-              ),
+            Column(
+              children: <Widget>[
+                SettingsTile(Icons.lock, 'Alterar senha'),
+                SettingsTile(Icons.question_answer, 'Duvidas'),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: MaterialButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => LoginScreen(),
-                    ),
-                  );
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(100)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 60.0, right: 60.0, top: 8.0, bottom: 8.0),
-                    child: Text(
-                      'Sair',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Desenvolvido com 💚 por terEnsino © 2020.',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => LoginScreen(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(100)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 60.0, right: 60.0, top: 8.0, bottom: 8.0),
+                        child: Text(
+                          'Sair',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
+              ],
+            )
           ],
         ),
       ),
