a17a30440027cc6aa4cd60ff40788bab22214dae
lucsscarvalho <lucas.carvalhocco@gmail.com>
2020-07-03 09:52:49 -0300

    add textFormField

--- ter_ensino_mobile/lib/tabs/create_post_tab.dart
+++ ter_ensino_mobile/lib/tabs/create_post_tab.dart
@@ -32,41 +32,116 @@ class _CreatePostTabState extends State<CreatePostTab> {
       ),
       backgroundColor: Colors.grey[900],
       body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(50),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.person_outline),
-                    Column(
+        child: Column(children: <Widget>[
+          Container(
+            height: 70,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30.0,
+                    child: Container(
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(
+                              'assets/images/img_default_profile.jpg'),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           'Lucas Carvalho',
-                          style: TextStyle(fontSize: 15),
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
                         ),
-                        Text('aprendiz em tempo integral')
+                        Text(
+                          'aprendiz em tempo integral',
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500),
+                        )
                       ],
-                    )
-                  ],
-                ),
+                    ),
+                  )
+                ],
               ),
             ),
-            Form(
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Form(
               child: Column(
-                children: <Widget>[],
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Título do ensinamento',
+                    style: TextStyle(color: Colors.white, fontSize: 22),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                          hintText: ' Dê um título ao seu ensinamento',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Form(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Conteúdo',
+                            style: TextStyle(color: Colors.white, fontSize: 22),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                style: TextStyle(color: Colors.black),
+                                decoration: InputDecoration(
+                                  hintText: 'Compartilhe seu conteúdo :)',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ]),
       ),
     );
   }
