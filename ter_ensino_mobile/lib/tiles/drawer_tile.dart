66ce8aff8634d06a11394ce4ddf2270f73fd8926
lucsscarvalho <lucas.carvalhocco@gmail.com>
2020-07-02 23:40:47 -0300

    create drawer_tile

---
+++ ter_ensino_mobile/lib/tiles/drawer_tile.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class DrawerTile extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final PageController controller;
+  final int page;
+
+  DrawerTile(
+    this.icon,
+    this.text,
+    this.controller,
+    this.page,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).pop();
+          controller.jumpToPage(page);
+        },
+        child: Container(
+            padding: EdgeInsets.only(left: 15),
+            height: 60.0,
+            child: Row(
+              children: <Widget>[
+                Icon(icon,
+                    size: 32,
+                    color: controller.page.round() == page
+                        ? Colors.green
+                        : Colors.black),
+                SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  text,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: controller.page.round() == page
+                        ? Colors.green
+                        : Colors.black,
+                  ),
+                )
+              ],
+            )),
+      ),
+    );
+  }
+}
