b3444ba2ef8aea3176cadd9c868deac4001c0dbc
lucsscarvalho <lucas.carvalhocco@gmail.com>
2020-07-07 20:05:57 -0300

    update homeScreen

--- ter_ensino_mobile/lib/screens/home_screen.dart
+++ ter_ensino_mobile/lib/screens/home_screen.dart
@@ -1,35 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:ter_ensino_mobile/tabs/create_post_tab.dart';
-import 'package:ter_ensino_mobile/tabs/home_tab.dart';
-import 'package:ter_ensino_mobile/tabs/profile_tab.dart';
-import 'package:ter_ensino_mobile/tabs/settings_tab.dart';
 import 'package:ter_ensino_mobile/widgets/custom_drawer.dart';
 
-class HomeScreen extends StatelessWidget {
-  final _pageController = PageController();
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: _pageController,
-      physics: NeverScrollableScrollPhysics(),
-      children: <Widget>[
-        DefaultTabController(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: DefaultTabController(
           length: 3,
           child: Scaffold(
-            body: HomeTab(),
-            drawer: CustomDrawer(_pageController),
             appBar: AppBar(
               actions: <Widget>[
                 IconButton(
                   icon: Icon(Icons.rate_review),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => CreatePostTab(_pageController),
-                      ),
-                    );
-                  },
+                  onPressed: () {},
                 )
               ],
               title: Text(
@@ -58,33 +48,21 @@ class HomeScreen extends StatelessWidget {
                 ),
               ]),
             ),
+            backgroundColor: Colors.grey[900],
+            body: TabBarView(children: <Widget>[
+              Column(
+                children: <Widget>[],
+              ),
+              Column(
+                children: <Widget>[],
+              ),
+              Column(
+                children: <Widget>[],
+              ),
+            ]),
           ),
         ),
-        Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Text(
-              'Perfil',
-              style: TextStyle(color: Colors.white),
-            ),
-            centerTitle: true,
-          ),
-          drawer: CustomDrawer(_pageController),
-          body: ProfileTab(),
-        ),
-        Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Text(
-              'Configurações',
-              style: TextStyle(color: Colors.white),
-            ),
-            centerTitle: true,
-          ),
-          drawer: CustomDrawer(_pageController),
-          body: SettingsTab(),
-        ),
-      ],
+      ),
     );
   }
 }
