d69320a899b85bf4b06c1237678cdca21684c964
lucsscarvalho <lucas.carvalhocco@gmail.com>
2020-07-09 23:32:23 -0300

    update user

--- ter_ensino_mobile/lib/domain/user/user.dart
+++ ter_ensino_mobile/lib/domain/user/user.dart
@@ -1,15 +1,30 @@
 class User {
+  int id;
+  String name;
+  String email;
+  Null title;
+  Null about;
+  Null imageURL;
+
   User({this.id, this.name, this.email, this.title, this.about, this.imageURL});
 
-  final int id;
-  final int name;
-  final String email;
-  final String title;
-  final String about;
-  final String imageURL;
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    title = json['title'];
+    about = json['about'];
+    imageURL = json['imageURL'];
+  }
 
-  String acceptanceTermDate;
-  String lastAccess;
-  String cookie;
-  DateTime cookieExpireDate;
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['title'] = this.title;
+    data['about'] = this.about;
+    data['imageURL'] = this.imageURL;
+    return data;
+  }
 }
